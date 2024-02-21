8981b49bbc086a86128ca505afeb3f40f3f6b53a
iu52023 <160110098+iu52023@users.noreply.github.com>
2024-02-21 20:36:13 +0300

    Create .cpp

---
+++ 21_02_24_Lesson/.cpp
@@ -0,0 +1,7 @@
+#include <iomanip>
+cout<<fixed<<setprecision(20)
+  cout<<scientific<<setprecision(20)
+  setw(20) - ширина поля выода (только на одну переменную)
+  left - прижмется к левому краю
+  
+  
