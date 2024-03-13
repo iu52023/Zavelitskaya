b956bc5cfecaa4a1e49e2abccd6d8b025cd7c17a
iu52023 <160110098+iu52023@users.noreply.github.com>
2024-03-13 19:08:48 +0300

    Update lab_003.cpp

--- lab_003.cpp
+++ lab_003.cpp
@@ -2,34 +2,30 @@
 
 int main()
 {
-    int valueN;
-    int valueM;
-    std::cout<<"Данная программа помогает найти сумму натуральных чисел (∑n), которые:\n - делятся на 5\n - не делятся на заданное пользователем число m\n";
-    std::cout<<"Введите натуральное число n (положительное целое число)\n";
-    std::cin>>valueN;
-    std::cout<<"Введите натуральное число m (положительное целое число), должно быть строго меньше n\n";
-    std::cin>>valueM;
-    if (valueM>=valueN)
-    {
-        std::cout<<"Данные не соответствуют условиям. Попробуйте снова\n";
-        return 0;
-    }
-    int massSourceNum[valueN];
-    int i = 0;
-    for (i;i<valueN;i++)
-    {
-        massSourceNum[i]=valueN-i;
-    }
-    int sum;
-    for (int j;j<i;j++)
-    {
-        if (massSourceNum[j]%5==0 && massSourceNum[j]%valueM!=0)
-        {
-            sum+=massSourceNum[j];
-        }
-    }
-    std::cout<<"Итоговая сумма: ";
-    std::cout<<sum<<"\n";
-    return 0;
+	setlocale(0, "Rus");
+	int valueN;
+	int valueM;
+	std::cout << "Данная программа помогает найти сумму натуральных чисел, которые:\n - делятся на 5\n - не делятся на заданное пользователем число m\n";
+	std::cout << "Введите натуральное число n (положительное целое число)\n";
+	std::cin >> valueN;
+	std::cout << "Введите натуральное число m (положительное целое число), должно быть строго меньше n\n";
+	std::cin >> valueM;
+	if (valueM >= valueN || valueM < 0 || valueM < 0)
+	{
+		std::cout << "Данные не соответствуют условиям. Попробуйте снова\n";
+		return 0;
+	}
+	int sum = 0;
+	for (int i = 5; i <= valueN; i += 5)
+	{
+		if (i % valueM !=0)
+		{
+			sum += i;
+		}
+	}
+
+
+	std::cout << "Итоговая сумма: ";
+	std::cout << sum << "\n";
+	return 0;
 }
-Можно шаг 5 + не задавать изначальный массив, а пробегаться по итоговому фору
